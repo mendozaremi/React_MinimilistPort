74a4613f871f3b7b797949750f861563600ae758
mendozaremi <mendozaremi@gmail.com>
2021-08-21 08:34:26 -0400

    fixed styling still need folder structering

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About';
-import Card from './components/Card';
 import Work from './components/Work';
 
 function App() {
